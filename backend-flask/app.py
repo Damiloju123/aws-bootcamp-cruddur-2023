728d44d50969f86ad2b077614de69e943b041818
Damiloju Olagbade <jdamiloju@gmail.com>
2023-03-06 16:41:50 +0000

    Cloudwatch

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -23,8 +23,8 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
 # ----------------X-Ray---------------------------
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+#from aws_xray_sdk.core import xray_recorder
+#from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 # Honeycomb
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -32,16 +32,29 @@ provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 
-# Rollbar-------------------------------
+# WatchTower-------------------------------
+import watchtower
+import logging
 from time import strftime
+
+# Configuring Logger to Use CloudWatch
+# LOGGER = logging.getLogger(__name__)
+# LOGGER.setLevel(logging.DEBUG)
+# console_handler = logging.StreamHandler()
+# cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+# LOGGER.addHandler(console_handler)
+# LOGGER.addHandler(cw_handler)
+# LOGGER.info("test log")
+
+# Rollbar-------------------------------
 import os
 import rollbar
 import rollbar.contrib.flask
 from flask import got_request_exception
 
 #----------------------------X-ray---------------
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
+#xray_url = os.getenv("AWS_XRAY_URL")
+#xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
 
 
 # Show this in the logs within the back-end flask app (STDOUT)
@@ -53,7 +66,7 @@ tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 
-XRayMiddleware(app, xray_recorder)
+# XRayMiddleware(app, xray_recorder)
 
 # Honeycomb
 # Initialize automatic instrumentation with Flask
@@ -70,6 +83,12 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+#@app.after_request
+#def after_request(response):
+#    timestamp = strftime('[%Y-%b-%d %H:%M]')
+#    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#    return response
 
 # Rollbar ------------------------------------
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -2,7 +2,8 @@ from datetime import datetime, timedelta, timezone
 from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
-      segment = xray_recorder.begin_segment('user_activities')
+      #segment = xray_recorder.begin_segment('user_activities')
+      
       model = {
           'errors': None,
           'data': None
@@ -20,11 +21,11 @@ class UserActivities:
           'expires_at': (now + timedelta(days=31)).isoformat()
         }]
         model['data'] = results  
-      subsegment = xray_recorder.begin_segment('mock-data')
-      dict = {
-        "now": now.isoformat(),
-        "results-size": len(model['data'])
-      }
-      subsegment.put_metadata('key', dict, 'namespace')
+      #subsegment = xray_recorder.begin_segment('mock-data')
+      #dict = {
+      #  "now": now.isoformat(),
+      #  "results-size": len(model['data'])
+      #}
+      #subsegment.put_metadata('key', dict, 'namespace')
 
       return model
