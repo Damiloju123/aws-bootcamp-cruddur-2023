bf6f9134592df7a6cb05f6df57a5f50bcb8c4706
Damiloju Olagbade <jdamiloju@gmail.com>
2023-03-11 20:31:57 +0000

    Revert "revert"
    This reverts commit 8fbbc06e856c7cf818efe08fcc102d5d2d334cad.

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -11,6 +11,8 @@ import MessageGroupPage from './pages/MessageGroupPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
 
+
+
 import {
   createBrowserRouter,
   RouterProvider
@@ -33,6 +35,26 @@ Amplify.configure({
   }
 });
 
+import { init as honeycombInit, startTrace } from 'honeycomb-beeline';
+
+honeycombInit({
+  writeKey: process.env.HONEYCOMB_API_KEY,
+  dataset: process.env.HONEYCOMB_DATASET_NAME,
+  serviceName: process.env.HONEYCOMB_SERVICE_NAME,
+});
+
+async function fetchData(url, options) {
+  const trace = startTrace({ name: 'fetchData' });
+
+  try {
+    const response = await fetch(url, options);
+    finishTrace(trace);
+    return response.json();
+  } catch (error) {
+    finishTrace(trace);
+    throw error;
+  }
+}
 
 const router = createBrowserRouter([
   {
@@ -82,3 +104,7 @@ function App() {
 }
 
 export default App;
+
+export  {
+  fetchData,
+};
